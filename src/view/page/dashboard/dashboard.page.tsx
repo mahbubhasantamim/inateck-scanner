b9e21ca7de304867d7fc5b19211e06a36a80c94b
Mahbub Hasan <mahbubhasan.pro@gmail.com>
2024-09-14 15:25:29 +0600

    inateck-scanner-sdk setup

--- src/view/page/dashboard/dashboard.page.tsx
+++ src/view/page/dashboard/dashboard.page.tsx
@@ -1,9 +1,13 @@
 import PageWrapper from "@/view/components/layout/page-wrapper"
+import { useDashboardController } from "./dashboard.controller"
 
 export const DashboardPage = () => {
+    const { connectScanner, data } = useDashboardController()
+    connectScanner()
     return (
         <PageWrapper hideFooter>
             <div className="text-center py-4">Dashboard</div>
+            <p>{data}</p>
         </PageWrapper>
     )
 }
