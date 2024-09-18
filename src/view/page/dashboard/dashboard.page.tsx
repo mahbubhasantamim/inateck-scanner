5eab479f78849828d167aac74817225a31825963
Mahbub Hasan <mahbubhasan.pro@gmail.com>
2024-09-18 23:03:24 +0600

    use-scan-detection

--- src/view/page/dashboard/dashboard.page.tsx
+++ src/view/page/dashboard/dashboard.page.tsx
@@ -1,53 +1,21 @@
 import PageWrapper from "@/view/components/layout/page-wrapper"
-import { useEffect, useState } from "react"
+import { useState } from "react"
+import useScanDetection from "use-scan-detection"
 
 export const DashboardPage = () => {
-    const [sanitizedBarcode, setSanitizedBarcode] = useState<string>("")
-    const [scanning, setScanning] = useState<boolean>(false)
+    const [barcode, setBarcode] = useState("")
 
-    useEffect(() => {
-        const handleKeyPress = (event: KeyboardEvent) => {
-            if (!scanning) {
-                setScanning(true)
-            }
-            const newBarcode = event.key
-
-            // Sanitize the new barcode directly before setting the state
-            const sanitized = newBarcode.slice(1, -2)
-
-            // Update the barcode state and sanitizedBarcode state together
-            if (sanitized.length > 5) {
-                setSanitizedBarcode(sanitized)
-            }
-
-            // Stop scanning after a short delay (end of barcode scan)
-            const timeout = setTimeout(() => {
-                setScanning(false)
-            }, 100) // Adjust delay based on your scanner's speed
-
-            return () => clearTimeout(timeout)
-        }
-
-        window.addEventListener("keydown", handleKeyPress)
-
-        // Cleanup event listener on component unmount
-        return () => {
-            window.removeEventListener("keydown", handleKeyPress)
-        }
-    }, [scanning])
-
-    const handleBarcodeScan = (sanitizedBarcode: string) => {
-        if (sanitizedBarcode.length > 5) {
-            console.log("Scanned Barcode:", sanitizedBarcode)
-        }
-    }
-
-    handleBarcodeScan(sanitizedBarcode)
+    useScanDetection({
+        onComplete: (code) => {
+            setBarcode(code as string)
+        },
+        minLength: 3,
+    })
 
     return (
         <PageWrapper hideFooter>
             <div className="text-center py-4">Dashboard</div>
-            <p>Barcode: {sanitizedBarcode}</p>
+            <p>Barcode: {barcode}</p>
         </PageWrapper>
     )
 }
